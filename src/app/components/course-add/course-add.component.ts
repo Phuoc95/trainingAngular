948b6fc2375646bcb2f939ef5129f501b18acf0a
phuoctk <duyphuoctk@gmail.com>
2019-09-23 23:07:22 +0700

    update function

--- src/app/components/course-add/course-add.component.ts
+++ src/app/components/course-add/course-add.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CourseService } from "./../../services/course.service";
+import { Subscription } from "rxjs";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-course-add',
-  templateUrl: './course-add.component.html',
-  styleUrls: ['./course-add.component.css']
+  selector: "app-course-add",
+  templateUrl: "./course-add.component.html",
+  styleUrls: ["./course-add.component.css"]
 })
 export class CourseAddComponent implements OnInit {
   public course = {};
+  public subscription: Subscription;
 
-  constructor() { }
+  constructor(
+    public courseService: CourseService,
+    public routerService: Router
+  ) {}
 
-  ngOnInit() {
-    
+  ngOnInit() {}
+
+  onAddCourse() {
+    this.subscription = this.courseService
+      .addCourse(this.course)
+      .subscribe(res => {
+        if (res && res.id) {
+            this.routerService.navigate(['/courses'])
+        }
+      });
   }
 
-  onSubmit() {
-    console.log(this.course, 99);
+  onDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
-
 }
--- src/app/components/course-edit/course-edit.component.ts
+++ src/app/components/course-edit/course-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from "./../../services/course.service";
+import { Subscription } from "rxjs";
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-edit',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-edit.component.css']
 })
 export class CourseEditComponent implements OnInit {
+  public subscription: Subscription;
+  public subscriptionParams: Subscription;
+  public course = {};
 
-  constructor() { }
+  constructor(
+    public courseService: CourseService,
+    public routerService: Router,
+    public activatedRouterService: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.onLoadData()
   }
 
+  onLoadData() {
+    // this.subscriptionParams = this.activatedRouterService.parent.params.subscribe(data => {
+    //   console.log(data,20);
+    // })
+    this.subscriptionParams = this.activatedRouterService.params.subscribe(data => {
+      this.subscription = this.courseService.getCourse(data['id']).subscribe(res => {
+        this.course = res;
+      })
+    })
+  }
+
+  onUpdateCourse() {
+    this.subscription = this.courseService.updateCourse(this.course.id, this.course)
+      .subscribe(res => {
+        if (res) {
+            this.routerService.navigate(['/courses'])
+        }
+      });
+  }
+
+  onDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.subscriptionParams) {
+      this.subscriptionParams.unsubscribe();
+    }
+  }
 }
